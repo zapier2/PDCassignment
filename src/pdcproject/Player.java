2e6a6008cd8564923f9c59b42e0b677ac93fed14
fengv1976 <69778760+fengv1976@users.noreply.github.com>
2020-09-08 15:44:47 +1200

    player
    a

--- src/pdcproject/Player.java
+++ src/pdcproject/Player.java
@@ -11,4 +11,24 @@ package pdcproject;
  */
 public class Player {
     
+    String playerName = "";
+    int winnings = 0;
+    
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+    public void setWinnings(int winnings) {
+        this.winnings = winnings;
+    }
+    public String getPlayerName() {
+        return playerName;
+    }
+    public int getWinnings() {
+        return winnings;
+    }
+    public void Player(String name,int money) {
+        this.winnings = money;
+        this.playerName = name;
+    }
+    
 }
